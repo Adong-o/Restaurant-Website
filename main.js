81ee7d2b76f9ff3c8ca389ea51fbf8469b4a34a3
Adong-o <adongo969@gmail.com>
2023-07-12 09:41:45 +0300

    Restaurant Website
    Made with html, css and javascript for some call functions and i used mailto function to be able to send emails about the form.

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var bookNowButton = document.getElementById("book-now-btn");
+    bookNowButton.addEventListener("click", function() {
+        window.location.href = "tel:+254757689592";
+    });
+});
+
+//submit
